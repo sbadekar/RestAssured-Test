5ee752ced5399041b557a07d0645038d6d25589b
sbadekar <sbadekar1992@gmail.com>
2018-12-11 12:48:52 +0530

    Refactored scripts, Added some additional Scenarios.

--- src/test/java/com/assignment/UserTest.java
+++ src/test/java/com/assignment/UserTest.java
@@ -1,19 +1,24 @@
 package com.assignment;
 
 import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.config.RestAssuredConfig;
 import com.jayway.restassured.http.ContentType;
-import com.jayway.restassured.path.json.JsonPath;
 import com.jayway.restassured.response.Response;
-import com.jayway.restassured.response.ResponseBody;
-import com.jayway.restassured.specification.RequestSpecification;
-import org.junit.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static com.assignment.constants.RxConstants.*;
+import static org.testng.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 /**
  * Test class for User API testing.
@@ -21,59 +26,158 @@ import static org.junit.Assert.assertTrue;
  */
 public class UserTest {
 
-    /**
-     * Test basic User API get using Hamcrest's matchers.
-     */
-    @Test
+    @BeforeClass
+    public void setUp() {
+        RestAssured.config = new RestAssuredConfig().encoderConfig(encoderConfig().defaultContentCharset("UTF-8"));
+        RestAssured.baseURI = baseURI;
+    }
+
+    @Test(description = "verify Get API Call ")
     public void testUserGet() {
-        RestAssured
-                .given().contentType(ContentType.JSON)
-                .when().get("https://reqres.in/api/users")
-                .then().statusCode(200)
+        given().contentType(ContentType.JSON)
+                .when().get("/users")
+                .then().statusCode(HTTP_STATUS_CODE_OK)
                 .and()
                 .contentType(ContentType.JSON)
-                .and().assertThat().body("data.first_name", hasItem("George"));
+                .and().assertThat().body("data.first_name", hasItem("George")).log().all();
     }
 
-
-
-    @Test
+    @Test(description = "verify Post API Call ")
     public void testUserCreation() {
-        Map<String,String> user = new HashMap<String, String>();
-        user.put("name", "sujata");
-        user.put("job", "automation");
+        Map<String,String> user = new HashMap<>();
+        user.put("name", "John");
+        user.put("job", "Software Engineer");
 
-        RestAssured
-                .given().contentType(ContentType.JSON)
+        given().contentType(ContentType.JSON)
                 .body(user)
-                .when().post("https://reqres.in/api/users")
+                .when().post("/users")
                 .then()
                 .contentType(ContentType.JSON)
-                .and().statusCode(201)
-                .and().assertThat().body("name", is("sujata"));
+                .and().statusCode(HTTP_STATUS_CODE_CREATED)
+                .and().assertThat().body("name", is("John")).log().all();
     }
 
-    @Test
+    @Test(description = "verify Put API Call ")
     public void testUserUpdate() {
-        Map<String,String> user = new HashMap<String, String>();
-        user.put("name", "badekar");
+        Map<String,String> user = new HashMap<>();
+        user.put("name", "Merry");
+
+        given().contentType(ContentType.JSON)
+                .body(user)
+                .when().put("/users/2")
+                .then()
+                .contentType(ContentType.JSON)
+                .and().statusCode(HTTP_STATUS_CODE_OK)
+                .and().assertThat().body("name", is("Merry")).log().all();;
+    }
+
+    @Test(description = "verify Delete API Call ")
+    public void testUserDeletion() {
+        given()
+                .when().delete("/users/2")
+                .then().statusCode(HTTP_STATUS_CODE_NO_CONTENT).log().all();;
+    }
+
+
+    //Additional Test Cases
+    @Test(description = "verify Get API Call for user which does not exist")
+    public void testUserGetInvalidUser() {
+        given().contentType(ContentType.JSON)
+                .when().get("/users/100")
+                .then().statusCode(HTTP_STATUS_CODE_NOT_FOUND).log().all();
+    }
+
+    @Test(description = "verify Get API Call for Per page records & total_pages calculation based on per page count ")
+    public void testUserGetperPageRecord() {
+        Response response =  RestAssured.given().contentType(ContentType.JSON)
+                .when().get("/users?per_page=1");
+        assertEquals(HTTP_STATUS_CODE_OK , response.statusCode());
+        User user = response.as(User.class);
+        assertEquals(user.getPage(), new Integer(1));
+        assertEquals(user.getTotal(), new Integer(12));
+        assertEquals(user.getTotal_pages(), new Integer(12));
+        assertEquals(1, user.getData().stream().count());
+    }
+
+    @Test(description = "verify Get API Call for all given query parameters for setting pagination")
+    public void testUserGetWithAllQueryParameters() {
+        Response response =  RestAssured.given().contentType(ContentType.JSON)
+                .when().get("/users?per_page=6&page=2&total=12&total_pages=2");
+        assertEquals(HTTP_STATUS_CODE_OK , response.statusCode());
+        User user = response.as(User.class);
+        List<Integer> idList= user.getData().stream().map(Employee :: getId).collect(Collectors.toList());
+        assertThat(idList, hasSize(6));
+        assertThat(idList, contains(7, 8, 9, 10, 11, 12));
+    }
+
+    @Test(description = "verify Get API Call for  maximum records limit crossed : it shows 12 only as total is 12 ")
+    public void testUserGetWithMaxLimit() {
+        RestAssured
+                .given().contentType(ContentType.JSON)
+                .when().get("/users?per_page=15")
+                .then().statusCode(HTTP_STATUS_CODE_OK)
+                .and()
+                .contentType(ContentType.JSON)
+                .and().assertThat().body("total", is(12))
+                .and().assertThat().body("total_pages", is(1))
+                .and().assertThat().body("data",hasSize(12)).log().all();
+    }
+
+    @Test(description = "verify Get API Call by giving page number which does not have record: Empty set returns ")
+    public void testUserGetWithNorecordPage() {
+        RestAssured
+                .given().contentType(ContentType.JSON)
+                .when().get("/users?page=13")
+                .then().statusCode(HTTP_STATUS_CODE_OK)
+                .and()
+                .contentType(ContentType.JSON)
+                .and().assertThat().body("data",hasSize(0)).log().all();
+    }
+
+    @Test(description = "verify Get API Call with invalid query parameters: Empty set returns ")
+    public void testUserGetWithInvalidQueryParameters() {
+        RestAssured
+                .given().contentType(ContentType.JSON)
+                .when().get("/users?per_page=-1&page=50&total=20&total_pages=100")
+                .then().statusCode(HTTP_STATUS_CODE_OK)
+                .and()
+                .contentType(ContentType.JSON)
+                .and().assertThat().body("data",hasSize(0)).log().all();
+    }
+
+     /*
+     Below scenarios tried for invalid datatype for other methods but seems no
+     validaton added so test cases are getting passed.
+    */
+     @Test(description = "verify Post API Call with invalid data: Users still getting created no validations ")
+    public void testUserCreationWithInvalidData() {
+        Map<Integer,Integer> user = new HashMap<>();
+        user.put(1,2);
+        user.put(3,4);
+        user.put(5,6);
+        user.put(7,8);
 
         RestAssured
                 .given().contentType(ContentType.JSON)
                 .body(user)
-                .when().put("https://reqres.in/api/users/2")
+                .when().post("/users")
                 .then()
                 .contentType(ContentType.JSON)
-                .and().statusCode(200)
-                .and().assertThat().body("name", is("badekar"));
+                .and().statusCode(HTTP_STATUS_CODE_CREATED).log().all();
     }
 
-    @Test
-    public void testUserDeletion() {
+    @Test(description = "verify Put API Call with invalid data: Still getting updated as no validation ")
+    public void testUserUpdateWithInvalidData() {
+        Map<Integer,Boolean> user = new HashMap<Integer, Boolean>();
+        user.put(5, false);
 
         RestAssured
-                .given()
-                .when().delete("https://reqres.in/api/users/2")
-                .then().statusCode(204);
+                .given().contentType(ContentType.JSON)
+                .body(user)
+                .when().put("/users/-1")
+                .then()
+                .contentType(ContentType.JSON)
+                .and().statusCode(HTTP_STATUS_CODE_OK).log().all();
     }
+
 }
---
+++ src/test/java/com/assignment/constants/RxConstants.java
@@ -0,0 +1,14 @@
+package com.assignment.constants;
+
+public interface RxConstants {
+
+	String baseURI = "https://reqres.in/api";
+
+	int HTTP_STATUS_CODE_CREATED = 201;
+	int HTTP_STATUS_CODE_OK = 200;
+	int HTTP_STATUS_CODE_ACCEPTED = 202;
+	int HTTP_STATUS_CODE_NO_CONTENT = 204;
+	int HTTP_STATUS_CODE_NOT_FOUND = 404;
+	int HTTP_STATUS_CODE_INTERNAL_SERVER_ERROR = 500;
+	int HTTP_STATUS_CODE_BAD_REQUEST = 400;
+}
